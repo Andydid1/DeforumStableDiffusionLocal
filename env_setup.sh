f601b864530e24062f339f26d466ff1565f3fcbe
Guandi Wang <guandiwang2023@u.northwestern.edu>
2022-10-25 00:49:32 -0500

    Setup Env

--- env_setup.sh
+++ env_setup.sh
@@ -1,3 +1,5 @@
+curl https://f004.backblazeb2.com/file/aai-blog-files/sd-v1-4.ckpt > sd-v1-4.ckpt
+mv sd-v1-4.ckpt ~/DeforumStableDiffusionLocal/models
 conda create -n dsm python=3.8.3
 conda activate dsm
 python setup.py
