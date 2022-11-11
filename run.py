3eaa80db2c1421dd2c03b65d7e0411f7aa9a748f
Guandi Wang <guandiwang2023@u.northwestern.edu>
2022-11-10 23:02:00 -0600

    Change output folder

--- run.py
+++ run.py
@@ -6,7 +6,7 @@
 
 import os, time
 def get_output_folder(output_path, batch_folder):
-    out_path = os.path.join(output_path,time.strftime('%Y-%m'))
+    out_path = os.path.join(output_path)
     if batch_folder != "":
         out_path = os.path.join(out_path, batch_folder)
     os.makedirs(out_path, exist_ok=True)
